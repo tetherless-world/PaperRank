3c207ea54a5baa355115b8589730f1a251bb7442
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-06-28 11:55:52 -0400

    Added skeleton and supporting architecture for unittest

---
+++ tests/basic_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from context import PaperRank
+
+class TestPaperRank(unittest.TestCase):
+    """Testing class for PaperRank basic behavior
+    """
+    def test_basic(self):
+        """Sample test
+        """
+
+        self.assertTrue(True)
