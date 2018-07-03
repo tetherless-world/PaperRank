b870968c34a43928c1c8db70ce64ff2495663aef
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-03 16:21:57 -0400

    Added unit test for query class

---
+++ tests/query_test.py
@@ -0,0 +1,26 @@
+from context import PaperRank
+
+import unittest
+
+
+class TestQuery(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """Override initialization function to setup PaperRank config.
+        """
+
+        super(TestQuery, self).__init__(*args, **kwargs)
+        PaperRank.util.configSetup()
+        self.config = PaperRank.util.config
+
+    def test_init(self):
+        """Test intialization of Query object.
+        """
+
+        PaperRank.util.configSetup()
+        config = PaperRank.util.config
+        db = PaperRank.util.Database(
+            host=self.config.test['redis']['host'],
+            port=self.config.test['redis']['port'],
+            db=self.config.test['redis']['db'])
+        test = PaperRank.update.Query(db=db, pmids=[21876761, 21876726])
+        self.assertTrue(True)
