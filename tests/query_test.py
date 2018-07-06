c7dad3442d5bf7d3665c651587cf1a28c1e15f3e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-06 17:48:59 -0400

    Refactored test - added count checking for better test resiliency

--- tests/query_test.py
+++ tests/query_test.py
@@ -4,6 +4,9 @@ import unittest
 
 
 class TestQuery(unittest.TestCase):
+    """Tests for the Query and Worker modules.
+    """
+
     def __init__(self, *args, **kwargs):
         """Override initialization function to setup PaperRank config.
         """
@@ -11,22 +14,25 @@ class TestQuery(unittest.TestCase):
         super(TestQuery, self).__init__(*args, **kwargs)
         PaperRank.util.configSetup()
         self.config = PaperRank.util.config
-
-    def test_init(self):
-        """Test intialization of Query object.
-        """
-
-        PaperRank.util.configSetup()
-        config = PaperRank.util.config
-        db = PaperRank.util.Database(
+        self.db = PaperRank.util.Database(
             host=self.config.test['redis']['host'],
             port=self.config.test['redis']['port'],
             db=self.config.test['redis']['db'])
+
+    def test_init(self):
+        """Test Query and Worker objects.
+        """
+
+        # Flush test database (manual access)
+        self.db.r.flushdb()
+
         # Testing Query module
-        PaperRank.update.Query(db=db,
-                               pmids=[21876761, 21876726,
-                                      29409535, 29025144],
-                               suppress_worker=True)
+        PaperRank.update.Query(db=self.db,
+                               pmids=[21876761, 21876726, 29409535, 29025144])
         # Testing behavior with invalid IDs
-        PaperRank.update.Query(db=db, pmids=['sdfgsdg'])
-        self.assertTrue(True)
+        PaperRank.update.Query(db=self.db, pmids=['sdfgsdg'])
+        
+        dangling_count = self.db.size(database='D')
+        seen_count = self.db.size(database='S')
+
+        self.assertEqual(dangling_count + seen_count, 5)
