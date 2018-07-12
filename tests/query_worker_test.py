bbecd7c354b2135f94fe47e3998271505711527e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-12 19:18:10 -0400

    Updated test to work with new architecture

--- tests/query_worker_test.py
+++ tests/query_worker_test.py
@@ -1,4 +1,7 @@
+# from context import PaperRank
 from context import PaperRank
+from multiprocessing import Manager, Pool
+from redis import ConnectionPool, StrictRedis
 
 import unittest
 
@@ -14,36 +17,51 @@ class TestQueryAndWorker(unittest.TestCase):
         super(TestQueryAndWorker, self).__init__(*args, **kwargs)
         PaperRank.util.configSetup()
         self.config = PaperRank.util.config
-        self.db = PaperRank.util.Database(
+
+        self.conn_pool = ConnectionPool(
             host=self.config.test['redis']['host'],
             port=self.config.test['redis']['port'],
-            db=self.config.test['redis']['db'])
+            db=self.config.test['redis']['db']
+        )
+
+        # Creating process pool
+        self.pool = Pool(processes=10, maxtasksperchild=10)
+
+        # Creating manager object, shared counter value and lock
+        self.m = Manager()
+        self.proc_count = self.m.Value('i', 0)
+        self.lock = self.m.Lock()
 
     def test_init(self):
         """Test Query and Worker objects.
         """
 
         # Flush test database (manual access)
-        self.db.r.flushdb()
+        r = StrictRedis(connection_pool=self.conn_pool)
+        r.flushdb()
 
         # Testing Query module
-        PaperRank.update.Query(db=self.db,
-                               pmids=[21876761, 21876726, 29409535, 29025144])
+        PaperRank.update.Query(conn_pool=self.conn_pool,
+                               pmids=[21876761, 21876726, 29409535, 29025144],
+                               proc_count=self.proc_count,
+                               lock=self.lock)
         # Testing behavior with invalid IDs
-        PaperRank.update.Query(db=self.db, pmids=['sdfgsdg'])
+        PaperRank.update.Query(conn_pool=self.conn_pool,
+                               pmids=['sdfgsdg'],
+                               proc_count=self.proc_count,
+                               lock=self.lock)
         
-        dangling_count = self.db.size(database='D')
-        seen_count = self.db.size(database='S')
+        dangling_count = r.scard(name='DANGLING')
+        seen_count = r.scard('SEEN')
 
         # Note: As this is a clean database, the number of
         # tuples added to `GRAPH` should be equal to the number of
         # IDs added to `EXPLORE`, as they are all not in `SEEN`. This implies
         # that their difference should be 0, and not affect the sum of the
         # number of IDs in `DANGLING` and the number of IDs in `SEEN`
-        explore_graph_diff = self.db.size(database='G') \
-            - self.db.size(database='E')
+        explore_graph_diff = r.scard(name='GRAPH') - r.scard(name='EXPLORE')
 
         self.assertEqual(dangling_count + seen_count + explore_graph_diff, 5)
 
         # Flush test database again (manual access)
-        self.db.r.flushdb()
+        r.flushdb()
