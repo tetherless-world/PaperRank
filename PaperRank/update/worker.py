3820488e95a912f91b50f4eecc921354454b7a23
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-12 13:21:56 -0400

    Changed worker class to function to queue actions in pipe object instead of executing immediately

--- PaperRank/update/worker.py
+++ PaperRank/update/worker.py
@@ -1,63 +1,58 @@
-from ..util import Database
 from .citation.ncbi_citation import NCBICitation as Citation
-import logging
+from collections import OrderedDict
+from redis.client import StrictPipeline
 
 
-class Worker:
-    def __init__(self, db: Database, citation: Citation):
-        """Worker class initialization. Adds tuples to GRAPH, maps outbound
-        citations in OUT, adds unseen IDs to EXPLORE, and adds and removes
-        current ID to and from INSTANCE.
-        
-        Arguments:
-            db {Database} -- Database to be used for data transactions.
-            citation {Citation} -- Citation object to be parsed.
-        """
+def worker(pipe: StrictPipeline, linkset: OrderedDict) -> StrictPipeline:
+    """Worker function. Queues actions in the `StrictPipeline` object to add
+    tuples to GRAPH, maps outbound citations in OUT, adds unseen IDs to EXPLORE
+    and removes current ID from INSTANCE.
+    
+    Arguments:
+        pipe {StrictPipeline} -- Pipeline object to be used for queueing.
+        linkset {OrderedDict} -- Raw response from the NCBI API.
+    
+    Returns:
+        StrictPipeline -- Modified `StrictPipeline` object with new actions
+                          queued.
+    """
 
-        self.db = db
-        self.citation = citation
+    # Create citation object
+    citation = Citation(query_raw=linkset)
 
-        if self.citation.error:
-            # Escape if there is an error
-            return
+    if citation.error:
+        # Escape, return unmodified pipe if there is an error
+        return pipe
+    
+    # Adding to 'SEEN'
+    pipe.sadd('SEEN', citation.id)
 
-        # Adding to `SEEN`
-        self.db.addMultiple(database='S', data=[self.citation.id])
+    # Building inbound and outbound tuples
+    out_tuples = ['("{0}","{1}")'.format(citation.id, i)
+                  for i in citation.outbound]
+    in_tuples = ['("{0}","{1}")'.format(i, citation.id)
+                 for i in citation.inbound]
 
-        # Building inbound and outbound tuples
-        out_tuples = ['("{0}","{1}")'.format(self.citation.id, i)
-                      for i in self.citation.outbound]
-        in_tuples = ['("{0}","{1}")'.format(i, self.citation.id)
-                     for i in self.citation.inbound]
+    if (len(out_tuples) + len(in_tuples)) > 0:
+        # Check if inbound or outbound citations exist
 
-        if (len(out_tuples) + len(in_tuples)) > 0:
-            # Check if inbound or outbound citations exist
+        # Adding tuples to `GRAPH`
+        pipe.sadd('GRAPH', *in_tuples, *out_tuples)
 
-            # Adding tuples to `GRAPH`
-            self.db.addMultiple(database='G', data=[*in_tuples, *out_tuples])
+        # Save outbound citations to `OUT`
+        pipe.hmset('OUT', {citation.id: citation.outbound})
 
-            # Save outbound citations to `OUT`
-            self.db.addMultiple(database='O',
-                                data={
-                                    self.citation.id: self.citation.outbound
-                                })
+        # Add all inbound and outbound IDs to EXPLORE
+        pipe.sadd('EXPLORE',
+                  *citation.inbound, *citation.outbound)
+        # Store the difference of `EXPLORE`` and `SEEN` in `EXPLORE`
+        pipe.sdiffstore('EXPLORE', 'EXPLORE', 'SEEN')
+    else:
+        # No inbound or outbound citations; add to `DANGLING`
+        pipe.sadd('DANGLING', citation.id)
+    
+    # Remove current ID from 'INSTANCE'
+    pipe.srem('INSTANCE', citation.id)
 
-            # Utilizing redis base class pipeline to execute commands in order
-            # see: https://bit.ly/2u0J96d
-
-            # Create pipeline object
-            pipe = self.db.r.pipeline()
-            # Add all inbound and outbound IDs to EXPLORE
-            pipe.sadd('EXPLORE',
-                      *self.citation.inbound, *self.citation.outbound)
-            # Store the difference of `EXPLORE`` and `SEEN` in `EXPLORE`
-            pipe.sdiffstore('EXPLORE', 'EXPLORE', 'SEEN')
-            # Execute commands
-            pipe.execute()
-
-        else:
-            # No inbound or outbound citations; add to `DANGLING`
-            self.db.addMultiple(database='D', data=[self.citation.id])
-
-        # Remove current ID from `INSTANCE`
-        self.db.removeMultiple(database='I', data=[self.citation.id])
+    # Return pipe object with new instructions
+    return pipe
