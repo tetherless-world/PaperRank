b38e3a35347fb3495874bd5d47dc258dffaa68b4
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-12 13:01:16 -0400

    Updated manager with new asynchronous multithreading logic

--- PaperRank/update/manager.py
+++ PaperRank/update/manager.py
@@ -1,23 +1,30 @@
-from ..util import config, Database
+from ..util import config
 from .query import Query
-from multiprocessing import Process
+from multiprocessing import Pool, Manager
 from time import sleep
 import logging
+import os
+import redis
 
 
 class Manager:
-    def __init__(self, db: Database, recover: bool=True):
+    def __init__(self, conn_pool: redis.ConnectionPool,
+                 recover: bool=True):
         """Manager class initialization. Loads configuration variables,
         and recovers gracefully from a crash by default.
         
         Arguments:
-            db {Database} -- Database to be used for data transactions.
+            conn_pool {ConnectionPool} -- Connection pool to be used for
+                                          Database transactions.
         
         Keyword Arguments:
             recover {bool} -- True for crash recovery (default: {True}).
         """
 
-        self.db = db
+        self.conn_pool = conn_pool
+
+        # Creating database object for the Manager
+        self.db = redis.Redis(connection_pool=self.conn_pool)
 
         # Recovering from failure
         if recover:
@@ -32,30 +39,56 @@ class Manager:
         """Function to start scraping.
         """
 
-        while not self.db.isEmpty(database='E'):
-            # Isolate PMIDs 
-            pmids = self.db.pop(database='E', n=self.pmid_per_second)
-            # Writing to instance nodes
-            self.db.addMultiple(database='I', data=pmids)
-            # Logging progress
-            logging.info('Extracted {0} PubMed IDs for scraping'
-                         .format(len(pmids)))
-            # Loop through until empty
-            queries = []
-            while len(pmids) > 0:
-                # Spawn thread with Query worker, and PMIDs
-                t = Process(target=Query, kwargs={
-                    'db': self.db,
-                    'pmids': pmids[0:self.pmid_per_request]
-                })
-                queries.append(t)
+        # Setting process pool size
+        pool_size = os.cpu_count()
+
+        # Creating process pool, limiting reuse
+        pool = Pool(process=pool_size, maxtasksperchild=10)
+
+        # Creating manager object, shared counter value, and lock
+        m = Manager()
+        proc_count = m.Value('i', 0)
+        lock = m.Lock()
+
+        # Getting initial exploration counter
+        explore_count = self.getExplorationCount()
+
+        while explore_count > 0:
+            # Check if process limit is not reached
+            if proc_count.value < pool_size:
+
+                # Get PMIDs
+                pmids = self.db.srandmemeber(
+                    name='EXPLORE',
+                    number=self.pmid_per_second)
+                # Removing popped elements
+                self.db.srem('EXPLORE', *pmids)
+                # Adding IDs to instance nodes
+                self.db.sadd('INSTANCE', *pmids)
+
                 # Logging progress
-                logging.info('Spawned Query thread with {0} PubMed IDs'
-                             .format(len(pmids[0:self.pmid_per_request])))
-                # Remove used PMIDs from list
-                del pmids[0:self.pmid_per_request]
-            [t.start() for t in queries]
-            [t.join() for t in queries]
+                logging.info('Extracted {0} PubMed IDs for scraping'
+                             .format(len(pmids)))
+
+                # Acquire lock for counter
+                lock.acquire()
+
+                # Create Query workers
+                while len(pmids) > 0:
+                    pool.apply_async(Query, (
+                        'conn_pool': self.conn_pool,
+                        'pmids': pmids[0:self.pmid_per_request]
+                    ))
+                    proc_count.value += 1
+                    del pmids[0:self.pmid_per_request]
+
+                # Release lock
+                lock.release()
+            
+            # Check if explore count is near 0, if so update
+            if explore_count < 2000:
+                explore_count = self.getExplorationCount()
+
             # Wait for 1 second
             sleep(1)
 
@@ -71,3 +104,13 @@ class Manager:
         pipe.delete('INSTANCE')
         # Execute commands
         pipe.execute()
+
+    def getExplorationCount(self) -> int:
+        """Get the number of elements remaining in the
+        exploration frontier.
+        
+        Returns:
+            int -- Number of IDs in the exploration frontier.
+        """
+
+        return int(self.db.scard('EXPLORE'))
