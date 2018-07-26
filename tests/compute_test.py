99559bb2aebf4359c8322dfd491237120bd28c44
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-26 16:39:02 -0400

    Added test for the (still unwritten) compute engine - will use this to run it and test

---
+++ tests/compute_test.py
@@ -0,0 +1,83 @@
+from context import PaperRank
+from redis import StrictRedis, ConnectionPool
+
+import unittest
+
+
+class TestCompute(unittest.TestCase):
+    """Test the compute module with a dummy example.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Override initialization function to clear database for the
+        PaperRank Compute test.
+        """
+
+        # Running superclass initialization
+        super(TestCompute, self).__init__(*args, **kwargs)
+
+        # Setting up PaperRank
+        PaperRank.util.configSetup()
+        self.config = PaperRank.util.config
+
+        # Connecting to redis
+        self.redis = StrictRedis(
+            host=self.config.test['redis']['host'],
+            port=self.config.test['redis']['port'],
+            db=self.config.test['redis']['db']
+        )
+
+        # Flush db, set up example
+        self.redis.flushdb()
+
+    def test_computeModel(self):
+        """Test the compute module by computing PageRakn for a sample testcase.
+
+        The test case computes PageRank for the graph with the structure:
+
+        - Graphical graph representation:
+                 [2] <--- [4]
+                /  ▲      /
+               /    \    /
+              ▼      \  ▼
+             [1] <--- [3]
+    
+        - `IN` database mappings:
+            - {1: [2, 3]}    (new)
+            - {2: [3, 4]}      |
+            - {3: [4]}         |
+            - {4: []}         old
+        
+        - Solution (stable):
+            - [1] -> .4571428855
+            - [2] -> .2571428458
+            - [3] -> .1714285572
+            - [4] -> .1142857114
+        
+        """
+
+        pages = [1, 2, 3, 4]
+
+        inbound_map = {
+            1: [2, 3],
+            2: [3, 4],
+            3: [4],
+            4: []
+        }
+
+        # Adding pages to `SEEN`
+        self.redis.sadd('SEEN', *[1, 2, 3, 4])
+
+        # Adding inbound citation map
+        self.redis.hmset('IN', inbound_map)
+
+        # Creating redis-py connection pool
+        conn_pool = ConnectionPool(
+            host=self.config.test['redis']['host'],
+            port=self.config.test['redis']['port'],
+            db=self.config.test['redis']['db']
+        )
+
+        # Run PaperRank compute
+
+        self.assertTrue(True)  # Temporary
