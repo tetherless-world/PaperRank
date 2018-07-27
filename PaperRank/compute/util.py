d8b13b8b306fa3d9b882cd8ccac7738354817a99
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-26 21:14:38 -0400

    Added progress logging for the buildOutDegreeMap function

--- PaperRank/compute/util.py
+++ PaperRank/compute/util.py
@@ -13,6 +13,14 @@ def buildOutDegreeMap(r: StrictRedis):
     # Getting iterator for 'OUT'
     out_iterator = r.hscan_iter('OUT')
 
+    out_count = r.hlen('OUT')
+
+    logging.info('Building out degree map for {0} IDs'.format(out_count))
+
+    # Progress tracking
+    count = 0
+    last_check = 0
+
     # Iterating through every element
     for out in out_iterator:
         # Defining out degree
@@ -21,3 +29,13 @@ def buildOutDegreeMap(r: StrictRedis):
         }
         # Add to database
         r.hmset('OUT_DEGREE', out_degree)
+        # Increment count
+        count += 1
+        # Log every ~10%
+        if (count - last_check) > (out_count / 10):
+            last_check = count
+            logging.info('Out degree map {0}% complete'.format(
+                round(count / out_count, 3) * 10))
+
+    logging.info('Finished building out degree map for {0} IDs'.format(
+        r.hlen('OUT_DEGREE')))
