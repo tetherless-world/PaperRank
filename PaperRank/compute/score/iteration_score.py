30b50b635a4d288bc99a0305620b919686816d6f
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-26 21:40:54 -0400

    Implemented iteration score computation, updated config with additional beta configuration

--- PaperRank/compute/score/iteration_score.py
+++ PaperRank/compute/score/iteration_score.py
@@ -1,1 +1,55 @@
-# PaperRank iteration compute goes here
+from ...util import config
+from redis import StrictRedis
+import numpy as np
+
+
+def computeIterationScore(r: StrictRedis, id_list: np.array) -> np.array:
+    """Function to compute and returnthe iteration PaperRank for a
+    given list of IDs.
+    
+    Arguments:
+        r {StrictRedis} -- StrictRedis object for database operations.
+        id_list {np.array} -- List of IDs for PaperRank computation.
+    
+    Returns:
+        np.array -- Array of scores, with position corresponding to
+                    function input parameter id_list.
+    """
+
+    # Getting function variables
+    N = id_list.size
+    beta = config.compute['beta']
+
+    scores = np.array(dtype=float)
+
+    # Iterate through IDs
+    for paper_id in id_list:
+        score = 0
+        inbound_list = eval(r.hget('IN', paper_id))
+
+        # Iterate through inbound citations
+        for inbound in inbound_list:
+            # Check if paperrank exists for inbound ID
+            if r.hexists('PaperRank', inbound):
+                # Getting PaperRank in outbound degree of inbound
+                inbound_pr = float(r.hget('PaperRank', inbound))
+                inbound_outdeg = float(r.hget('OUT_DEGREE', inbound))
+                # Add to score
+                score += (beta * (inbound_pr / inbound_outdeg))
+            else:
+                logging.warn('No PaperRank for {0} inbound ID {1}. Skipping...'
+                             .format(paper_id, inbound))
+        # Save score to database
+        r.hset('PaperRank', {paper_id: score})
+
+        # Append score to score list
+        np.append(scores, score)
+    
+    # Redistribute leaked PaperRanks (from dangling papers)
+    
+    leaked_pr = 1 - np.sum(scores)
+
+    if leaked_pr > 0:
+        scores = scores + (leaked_pr / N)
+    
+    return scores
