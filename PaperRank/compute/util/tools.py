6b426eaeb5311f571be6e3963f2f6b14752860b4
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-27 12:13:52 -0400

    Refactored buildOutDegreeMap to intelligently build map only if necessary, and only for IDs that do not exist without iterating over entire list every time

--- PaperRank/compute/util/tools.py
+++ PaperRank/compute/util/tools.py
@@ -1,5 +1,6 @@
 from redis import StrictRedis
 import logging
+import numpy as np
 
 
 def buildOutDegreeMap(r: StrictRedis):
@@ -10,32 +11,40 @@ def buildOutDegreeMap(r: StrictRedis):
         r {StrictRedis} -- StrictRedis object for database operations.
     """
 
-    # Getting iterator for 'OUT'
-    out_iterator = r.hscan_iter('OUT')
+    # Check if out degree map refresh is necessary, if not return
+    if r.hlen('OUT') == r.hlen('OUT_DEGREE'):
+        return
 
-    out_count = r.hlen('OUT')
+    # Get list of elements in OUT and OUT_DEGREE
+    out_list = [i.decode('utf-8') for i in r.hkeys('OUT')]
+    outdeg_list = [i.decode('utf-8') for i in r.hkeys('OUT_DEGREE')]
 
-    logging.info('Building out degree map for {0} IDs'.format(out_count))
+    # Isolate elements that do not have OUT_DEGREE
+    missing = np.setdiff1d(out_list, outdeg_list)
+
+    missing_count = missing.size
+
+    logging.info('Building out degree map for {0} IDs'.format(missing_count))
 
     # Progress tracking
     count = 0
     last_check = 0
 
     # Iterating through every element
-    for out in out_iterator:
+    for missing_id in missing:
         # Defining out degree
         out_degree = {
-            out[0]: len(eval(out[1]))
+            missing_id: len(eval(r.hget('OUT', missing_id)))
         }
         # Add to database
         r.hmset('OUT_DEGREE', out_degree)
         # Increment count
         count += 1
         # Log every 0.5%
-        if (count - last_check) > (out_count / 100 * 0.5):
+        if (count - last_check) > (missing_count / 100 * 0.5):
             last_check = count
             logging.info('Out degree map {0}% complete'.format(
-                round(count / out_count, 3) * 100))
+                round(count / missing_count, 3) * 100))
 
     logging.info('Finished building out degree map for {0} IDs'.format(
         r.hlen('OUT_DEGREE')))
