1e2411d3be0a193aab200e5b0e53c3e989a2f720
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-27 01:09:46 -0400

    Major refactor, and added test for computeStableScore

--- tests/compute_score_test.py
+++ tests/compute_score_test.py
@@ -24,51 +24,52 @@ class TestComputeScore(unittest.TestCase):
             db=self.config.test['redis']['db']
         )
 
-        # Flush db, set up example data
-        self.redis.flushdb()
-
-    def test_computeIterationScore(self):
-        """Test the `computeIterationScore` function from the `score` submodule.
-
-        The test case is the following sample citation graph:
-
-        - Graphical graph representation:
-                 [2] <--- [4]
-                /  ▲      /
-               /    \    /
-              ▼      \  ▼
-             [1] <--- [3]
-
-        - `IN` database mappings:
-            - {1: [2, 3]}    (new)
-            - {2: [3, 4]}      |
-            - {3: [4]}         |
-            - {4: []}        (old)
+    def dataSetup(self) -> np.array:
+        """Function to setup the test case described below.
         
-        - `OUT` database mappings:
-            - {1: []}
-            - {2: [1]}
-            - {3: [1, 2]}
-            - {4: [2, 3]}
-        
-        - Artificial PaperRanks (inserted)
-            - {1: (this is what we compute)}
-            - {2: .5}
-            - {3: .25}
-            - {4: .25}
-        
-        - Expected solution
-            - {1: } (figure this out)
+            The test case is the following sample citation graph:
+
+            - Graphical graph representation:
+            
+                      [2] <--- [4]
+                     /  ▲      /
+                    /    \    /
+                   ▼      \  ▼
+                  [1] <--- [3]
+
+            - `IN` database mappings:
+                - {1: [2, 3]}    (new)
+                - {2: [3, 4]}      |
+                - {3: [4]}         |
+                - {4: []}        (old)
+            
+            - `OUT` database mappings:
+                - {1: []}
+                - {2: [1]}
+                - {3: [1, 2]}
+                - {4: [2, 3]}
+            
+            - Artificial PaperRanks (inserted)
+                - {2: .5}
+                - {3: .25}
+                - {4: .25}
+
+
+        Returns:
+            np.array -- Array of IDs.
         """
 
+        # Flush db
+        self.redis.flushdb()
+
         # Setting up sample data
-        inbound_map = {
+        self.inbound_map = {
             1: [2, 3],
             2: [3, 4],
             3: [4],
             4: []
         }
-        self.redis.hmset('IN', inbound_map)
+        self.redis.hmset('IN', self.inbound_map)
         outbound_map = {
             1: [],
             2: [1],
@@ -76,22 +77,24 @@ class TestComputeScore(unittest.TestCase):
             4: [2, 3]
         }
         self.redis.hmset('OUT', outbound_map)
-        old_scores = {
+        self.old_scores = {
             2: .5,
             3: .25,
             4: .25
         }
-        self.redis.hmset('PaperRank', old_scores)
-        out_degree = {
-            1: 0,
-            2: 1,
-            3: 2,
-            4: 2
-        }
-        id_list = np.array([1, 2, 3, 4], dtype=str)
+        self.redis.hmset('PaperRank', self.old_scores)
 
         # Running util to set up out degree map
         PaperRank.compute.util.buildOutDegreeMap(r=self.redis)
+
+        return np.array([1, 2, 3, 4], dtype=str)
+
+    def test_computeIterationScore(self):
+        """Test the `computeIterationScore` function from the `score` submodule.
+        """
+
+        # Setting up test environment
+        id_list = self.dataSetup()
 
         # Running iteration compute function
         scores = PaperRank.compute.score.computeIterationScore(
@@ -100,16 +103,24 @@ class TestComputeScore(unittest.TestCase):
         # Test verification
         # (Computing score manually)
 
+        out_degree = {
+            1: 0,
+            2: 1,
+            3: 2,
+            4: 2
+        }
+
         def manual_compute():
             beta = self.config.compute['beta']
             expected_scores = []
             id_list = [1, 2, 3, 4]
             for paper_id in id_list:
                 score = 0
-                inbound_list = inbound_map[paper_id]
+                inbound_list = self.inbound_map[paper_id]
 
                 for inbound in inbound_list:
-                    score += old_scores[inbound] / out_degree[inbound] * beta
+                    score += self.old_scores[inbound] \
+                        / out_degree[inbound] * beta
 
                 expected_scores.append(score)
             leaked_pr = 1 - sum(expected_scores)
@@ -121,3 +132,22 @@ class TestComputeScore(unittest.TestCase):
         expected_scores = manual_compute()
 
         self.assertListEqual(list(scores), expected_scores)
+
+    def test_computeStableScore(self):
+        """Test the `calculate` function from the `score` submodule.
+        """
+
+        # Setting up the test environment
+        id_list = self.dataSetup()
+
+        # Running stable compute function
+        PaperRank.compute.score.calculate(r=self.redis, id_list=id_list)
+
+        # Ensuring probability distribution by checking if sum is 1
+        scores_raw = self.redis.hgetall('PaperRank')
+        scores = np.array([], dtype=float)
+        for k, v in scores_raw.items():
+            scores = np.append(scores, v)
+        scores = np.array(scores).astype(np.float)
+
+        self.assertEqual(np.sum(scores), 1.0)
