6cdea838f791e7f48eed4def6dd8d8bead46912b
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-29 16:51:14 -0400

    Updated to allow tests to be run from command line

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../'))
-
-import PaperRank
+try:
+    import PaperRank
+except ModuleNotFoundError:
+    sys.path.insert(0, os.path.abspath('../'))
+    os.chdir(os.path.abspath('../'))
+    import PaperRank
