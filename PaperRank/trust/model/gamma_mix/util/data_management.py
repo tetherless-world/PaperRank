fc692692fa924ca9bed58c067a68974bc1ddc235
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-08-22 15:55:43 -0400

    Added funciton to compute the location parameter for a gamma distribution given data and scaled data

--- PaperRank/trust/model/gamma_mix/util/data_management.py
+++ PaperRank/trust/model/gamma_mix/util/data_management.py
@@ -25,3 +25,35 @@ def shiftData(data: np.array, shift: np.float=None) -> np.array:
         shift = config.trust['gamma_shift_constant']
 
     return data + np.absolute(np.min(data)) + shift
+
+
+def computeLocation(data: np.array, data_shifted: np.array,
+                    shift: np.float=None) -> np.float:
+    """Computes the location parameter, given data, shifted data, and the shift
+    parameter. The location parameter is the value by which the data is shifted
+    before being passed into the Gamma distribution function. It is set equal
+    to the following (also equivalent to min(data) - shift):
+    loc = min(shifted data) - abs(min(data)) - (2 * shift)
+    
+    Arguments:
+        data {np.array} -- Original data.
+        data_shifted {np.array} -- Shifted data.
+    
+    Keyword Arguments:
+        shift {np.float} -- Shift constant added to the data to enforce
+                            strit positivity  (default: {None}).
+    
+    Returns:
+        np.float -- Location parameter for the Gamma distribution.
+    """
+
+    # If data and data_shifted are equal, location is 0
+    if np.array_equal(data, data_shifted):
+        return 0
+
+    if not shift:
+        shift = config.trust['gamma_shift_constant']
+    
+    # Compute and return location
+    return np.min(data_shifted) - np.absolute(np.min(data)) \
+        - (2 * shift_constant)
