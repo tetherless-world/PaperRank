2b67fed27c3c0bc12bde6990ff54b75206f4f488
Jamie McCusker <mccusker@gmail.com>
2021-02-05 12:40:17 -0500

    Slight optimizations, needs testing

--- tests/citation_test.py
+++ tests/citation_test.py
@@ -20,7 +20,6 @@ i = 0
 inbound_map = {}
 outbound_map = {}
 seen_id = set()
-seen = []
 
 for fileName in inputFiles:
     strippedFile = fileName.rstrip()
@@ -31,48 +30,25 @@ for fileName in inputFiles:
         
         #create citation of loaded data
         cite = Citation(x)
+        # or reconstitute it using hex(cite.id)[2:-1]
+        cite.identifier = cite.id
+        cite.id = int(cite.id,16)
         
         #map id to integer
+        if cite.id in seen_id:
+            continue
         seen_id.add(cite.id)
-        id_map[i] = cite.id
-        rev_id_map[cite.id] = i
-        i = i+1
-
-        inbounds = cite.inbound
-        outbounds = cite.outbound
-
-        int_inbound = []
-        int_outbound = []
-
-        if len(inbounds) != 0:
-            for ins in inbounds:
-                if not(ins in seen_id):
-                    seen_id.add(ins)
-                    id_map[i] = ins
-                    rev_id_map[ins] = i
-                    i = i+1
-                int_inbound.append(rev_id_map[ins])
-        
-        if len(outbounds) != 0:
-            for out in inbounds:
-                if not(out in seen_id):
-                    seen_id.add(out)
-                    id_map[i] = out
-                    rev_id_map[out] = i
-                    i = i+1
-                int_outbound.append(rev_id_map[out])
         
         #update inbound and outbound lists
-        inbound_map[rev_id_map[cite.id]] = int_inbound
-        outbound_map[rev_id_map[cite.id]] = int_outbound
-        seen.append(rev_id_map[cite.id])
+        inbound_map[cite.id] = [int(x,16) for x in cite.inbound]
+        outbound_map[cite.id] = [int(x,16) for x in cite.outbound]
 
 # Setting up configuration
 PaperRank.util.configSetup(override='base.json')
 config = PaperRank.util.config
 
 # Creating manager
-compute_engine = PaperRank.compute.Manager(r=outbound_map, r_in= inbound_map, seenset = seen)
+compute_engine = PaperRank.compute.Manager(r=outbound_map, r_in= inbound_map, seenset = seen_id)
 
 # Run compute engine
 output = compute_engine.start(export = False)
